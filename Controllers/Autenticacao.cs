caa319d4f419d671cbc6305264c3546dde427854
21Programe <diego21question@gmail.com>
2023-10-15 18:43:18 -0300

    Criação do projeto

---
+++ Controllers/Autenticacao.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Biblioteca.Controllers
+{
+    public class Autenticacao
+    {
+        public static void CheckLogin(Controller controller)
+        {   
+            if(string.IsNullOrEmpty(controller.HttpContext.Session.GetString("user")))
+            {
+                controller.Request.HttpContext.Response.Redirect("/Home/Login");
+
+
+
+
+
+
+            }
+     
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
